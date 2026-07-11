1d5e0b95dcee648a7512eae231cc15bc278304d1
ggh66200-cpu <ggh66200@gmail.com>
2026-07-12 01:55:00 +0300

    Create langs.py

---
+++ langs.py
@@ -0,0 +1,24 @@
+LANG = {
+    'ar': {
+        'buy': '⚖️ شراء من زبون', 
+        'sell': '💰 بيع للزبون', 
+        'settings': '☀️ الإعدادات الصباحية',
+        'lang': '🌐 تغيير اللغة', 
+        'sub': '💳 الاشتراك'
+    },
+    'ku': {
+        'buy': '⚖️ کڕینی زێڕ', 
+        'sell': '💰 فرۆشتنی زێڕ', 
+        'settings': '☀️ ڕێکخستنی بەیانیان',
+        'lang': '🌐 گۆڕینی زمان', 
+        'sub': '💳 بەشداریکردن'
+    },
+    'en': {
+        'buy': '⚖️ Buy Gold', 
+        'sell': '💰 Sell Gold', 
+        'settings': '☀️ Morning Settings',
+        'lang': '🌐 Language', 
+        'sub': '💳 Subscription'
+    }
+}
+
