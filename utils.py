7cfe0eb37ac9c554499b1a3fe5d09f84efed50f8
ggh66200-cpu <ggh66200@gmail.com>
2026-07-12 23:54:07 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ DATA_FILE = 'data.json'
 
 def get_data():
     if not os.path.exists(DATA_FILE):
-        return {"subs": {}, "users": {}, "total_count": 0, "usd_100": 150000}
+        return {"subs": {}, "users": {}, "total_count": 0}
     with open(DATA_FILE, 'r', encoding='utf-8') as f:
         return json.load(f)
 
@@ -14,35 +14,22 @@ def save_data(data):
     with open(DATA_FILE, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-# تسجيل دخول مستخدم جديد وتفعيل الفترة المجانية
-def register_user(user_id):
-    data = get_data()
-    if str(user_id) not in data['users']:
-        data['users'][str(user_id)] = {"usage": 0, "trial_end": time.time() + (3 * 86400)}
-        data['total_count'] += 1
-        save_data(data)
-    return data['users'][str(user_id)]
-
-# فحص الوصول (100 مستخدم مجاني -> 3 أيام تجربة -> اشتراك)
 def check_access(user_id):
     data = get_data()
-    # إذا عدد المستخدمين أقل من 100، الكل مجاني
-    if data['total_count'] <= 100: return True
-    
-    # فحص الاشتراك المدفوع
-    if time.time() < data['subs'].get(str(user_id), 0): return True
-    
-    # فحص الفترة التجريبية (3 أيام)
-    user = data['users'].get(str(user_id))
-    if user and time.time() < user['trial_end']: return True
-    
+    uid = str(user_id)
+    # إذا لم يصل العدد لـ 100، الكل مجاني
+    if data['total_count'] < 100: return True
+    # فحص الاشتراك
+    if time.time() < data['subs'].get(uid, 0): return True
+    # إذا انتهى الاشتراك أو الفترة التجريبية
     return False
 
-def add_subscription(user_id, days):
+def register_user(user_id):
     data = get_data()
-    data['subs'][str(user_id)] = time.time() + (int(days) * 86400)
-    save_data(data)
-
-def get_stats():
-    data = get_data()
-    return f"عدد المستخدمين الكلي: {data['total_count']}\nالمشتركين الفعليين: {len(data['subs'])}"
+    uid = str(user_id)
+    if uid not in data['users']:
+        data['total_count'] += 1
+        data['users'][uid] = {"usage": 0}
+        save_data(data)
+        return True # مستخدم جديد
+    return False
