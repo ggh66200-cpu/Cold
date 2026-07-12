28163119c5dc5081b08667e261fdfb7412f34e5a
ggh66200-cpu <ggh66200@gmail.com>
2026-07-12 23:56:12 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,22 +1,21 @@
 import telebot
 import utils
 
-ADMIN_TOKEN = "8688685639:AAFbZRFFPKoooYAFCbPHLxfH7l9DWt6enFg"
-bot = telebot.TeleBot(ADMIN_TOKEN)
-ADMIN_ID = 7305704935# آيديك أنت
+bot = telebot.TeleBot("8688685639:AAFbZRFFPKoooYAFCbPHLxfH7l9DWt6enFg")
+ADMIN_ID = 7305704935 # آيديك
 
 @bot.message_handler(commands=['stats'])
 def stats(m):
     if m.chat.id == ADMIN_ID:
-        bot.reply_to(m, utils.get_stats())
+        data = utils.get_data()
+        bot.reply_to(m, f"📊 عدد المستخدمين: {data['total_count']}\nعدد المشتركين: {len(data['subs'])}")
 
-@bot.message_handler(commands=['approve'])
-def approve(m):
+@bot.message_handler(commands=['add'])
+def add(m):
     if m.chat.id == ADMIN_ID:
-        # /approve ID 30
+        # الصيغة: /add آيدي أيام
         parts = m.text.split()
         utils.add_subscription(parts[1], parts[2])
-        bot.reply_to(m, "✅ تم تفعيل الاشتراك.")
+        bot.reply_to(m, "✅ تم التفعيل.")
 
-# هنا تضيف أي أمر تحكم تحتاجه
 bot.polling()
