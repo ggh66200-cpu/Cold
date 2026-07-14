0e1ff351115800a1c5ffa11b870100e0cdc98ac4
ggh66200-cpu <ggh66200@gmail.com>
2026-07-14 14:26:13 +0300

    Implement default data generation in get_data
    Add default data creation if data file is missing.

--- utils.py
+++ utils.py
@@ -3,6 +3,21 @@ import json, os, time
 DATA_FILE = 'data.json'
 
 def get_data():
+    if not os.path.exists(DATA_FILE):
+        # إنشاء ملف افتراضي إذا لم يكن موجوداً
+        default_data = {
+            "total_count": 166,
+            "users": {},
+            "settings": {
+                "mithqal_21": 450000,
+                "mithqal_18": 380000,
+                "labor_21": 10000,
+                "labor_18": 10000,
+                "usd_100": 150000
+            }
+        }
+        save_data(default_data)
+        return default_data
     with open(DATA_FILE, 'r', encoding='utf-8') as f: 
         return json.load(f)
 
@@ -13,14 +28,15 @@ def save_data(data):
 def check_user(user_id):
     data = get_data()
     uid = str(user_id)
+    
+    # إذا كان مستخدم جديد تماماً
     if uid not in data['users']:
         data['total_count'] += 1
-        # تسجيل وقت الدخول لبدء الـ 7 أيام (مخزنة بالثواني)
         data['users'][uid] = {"join_date": time.time()}
         save_data(data)
         return True, data['total_count']
     
-    # فحص الاشتراك (7 أيام = 604800 ثانية)
+    # فحص الاشتراك التجريبي (7 أيام = 604800 ثانية)
     join_date = data['users'][uid]['join_date']
     if time.time() - join_date > 604800:
         return False, data['total_count'] # انتهى الاشتراك
