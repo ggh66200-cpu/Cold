2cd22b24f83cb905f48e25e447598b44310b2d29
ggh66200-cpu <ggh66200@gmail.com>
2026-07-15 14:23:14 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,25 +1,50 @@
 import json, os, time
+from telebot import types
 
 DATA_FILE = 'data.json'
 
 def get_data():
+    default_settings = {
+        "mithqal_21": 450000,
+        "mithqal_18": 380000,
+        "labor_21": 10000,
+        "labor_18": 10000,
+        "usd_100": 150000
+    }
+    
     if not os.path.exists(DATA_FILE):
-        # إنشاء ملف افتراضي إذا لم يكن موجوداً
         default_data = {
             "total_count": 166,
             "users": {},
-            "settings": {
-                "mithqal_21": 450000,
-                "mithqal_18": 380000,
-                "labor_21": 10000,
-                "labor_18": 10000,
-                "usd_100": 150000
-            }
+            "settings": default_settings
         }
         save_data(default_data)
         return default_data
-    with open(DATA_FILE, 'r', encoding='utf-8') as f: 
-        return json.load(f)
+        
+    try:
+        with open(DATA_FILE, 'r', encoding='utf-8') as f: 
+            data = json.load(f)
+    except:
+        data = {
+            "total_count": 166,
+            "users": {},
+            "settings": default_settings
+        }
+
+    if 'settings' not in data:
+        data['settings'] = default_settings
+    else:
+        for k, v in default_settings.items():
+            if k not in data['settings']:
+                data['settings'][k] = v
+                
+    if 'total_count' not in data:
+        data['total_count'] = 166
+    if 'users' not in data:
+        data['users'] = {}
+        
+    save_data(data)
+    return data
 
 def save_data(data):
     with open(DATA_FILE, 'w', encoding='utf-8') as f: 
@@ -29,17 +54,24 @@ def check_user(user_id):
     data = get_data()
     uid = str(user_id)
     
-    # إذا كان مستخدم جديد تماماً
-    if uid not in data['users']:
-        data['total_count'] += 1
+    # 🔥 الترميم التلقائي وحماية البيانات القديمة:
+    # إذا كان الحساب غير مسجل، أو مسجل بصيغة قديمة تالفة (ليست قاموساً)
+    if uid not in data['users'] or not isinstance(data['users'][uid], dict):
+        if uid not in data['users']:
+            data['total_count'] += 1
         data['users'][uid] = {"join_date": time.time()}
         save_data(data)
         return True, data['total_count']
     
-    # فحص الاشتراك التجريبي (7 أيام = 604800 ثانية)
-    join_date = data['users'][uid]['join_date']
-    if time.time() - join_date > 604800:
-        return False, data['total_count'] # انتهى الاشتراك
+    # إذا كان الحساب مسجلاً كقاموس ولكنه يفتقد تاريخ الانضمام
+    user_record = data['users'][uid]
+    if 'join_date' not in user_record:
+        user_record['join_date'] = time.time()
+        save_data(data)
+    
+    join_date = user_record['join_date']
+    if time.time() - join_date > 604800: # 7 أيام بالثواني
+        return False, data['total_count']
     
     return True, data['total_count']
 
@@ -47,3 +79,12 @@ def update_setting(key, value):
     data = get_data()
     data['settings'][key] = int(value)
     save_data(data)
+
+def get_keyboard():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("💰 بيع للزبون", "⚖️ شراء من زبون")
+    markup.add("⚙️ إعدادات الصباح")
+    return markup
+
+def send_main_menu(bot, chat_id, text_to_send):
+    bot.send_message(chat_id, text_to_send, parse_mode="Markdown", reply_markup=get_keyboard())
