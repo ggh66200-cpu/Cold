46b53f227e63fa20cb056b5ad25a66966d470efa
ggh66200-cpu <ggh66200@gmail.com>
2026-07-16 20:57:31 +0300

    Create subscription.py

---
+++ subscription.py
@@ -0,0 +1,21 @@
+import time, utils
+
+def check_user(user_id):
+    data = utils.get_data()
+    uid = str(user_id)
+    
+    if uid not in data['users'] or not isinstance(data['users'][uid], dict):
+        if uid not in data['users']:
+            data['total_count'] += 1
+        data['users'][uid] = {"join_date": time.time()}
+        utils.save_data(data)
+        return True, data['total_count']
+    
+    user_record = data['users'][uid]
+    if 'join_date' not in user_record:
+        user_record['join_date'] = time.time()
+        utils.save_data(data)
+    
+    # حالياً الاشتراك مفتوح للفحص، تقدر ترجع تقفله مستقبلاً من هنا
+    return True, data['total_count']
+  
