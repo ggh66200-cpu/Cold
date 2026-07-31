3facd944b4d05139feead5f04b41aa5b60d020b6
ggh66200-cpu <ggh66200@gmail.com>
2026-07-31 04:25:49 +0300

    Delete langs.py

--- langs.py
+++
@@ -1,5 +0,0 @@
-TEXTS = {
-    'ar': {'buy': '⚖️ شراء', 'sell': '💰 بيع', 'settings': '☀️ الإعدادات'},
-    'ku': {'buy': '⚖️ کڕین', 'sell': '💰 فرۆشتن', 'settings': '☀️ ڕێکخستن'},
-    'en': {'buy': '⚖️ Buy', 'sell': '💰 Sell', 'settings': '☀️ Settings'}
-}
